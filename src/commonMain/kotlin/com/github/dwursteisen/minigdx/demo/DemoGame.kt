558a831db134edfae5311f734eb44cf18a35e8af
David Wursteisen <david.wursteisen+git@gmail.com>
2020-05-03 18:02:06 +0200

    [COMMON] Add score on the game GUI

--- src/commonMain/kotlin/com/github/dwursteisen/minigdx/demo/DemoGame.kt
+++ src/commonMain/kotlin/com/github/dwursteisen/minigdx/demo/DemoGame.kt
@@ -7,8 +7,10 @@ import com.github.dwursteisen.minigdx.entity.CanDraw
 import com.github.dwursteisen.minigdx.entity.CanMove
 import com.github.dwursteisen.minigdx.entity.behavior.JumpBehavior
 import com.github.dwursteisen.minigdx.entity.delegate.Model
+import com.github.dwursteisen.minigdx.entity.models.Camera2D
 import com.github.dwursteisen.minigdx.entity.models.Camera3D
 import com.github.dwursteisen.minigdx.entity.models.Cube
+import com.github.dwursteisen.minigdx.entity.text.Text
 import com.github.dwursteisen.minigdx.fileHandler
 import com.github.dwursteisen.minigdx.graphics.clear
 import com.github.dwursteisen.minigdx.input.Key
@@ -26,7 +28,7 @@ class Player(private val model: Cube = Cube("player")) : CanMove by model, CanDr
         isJumping = { inputs.isKeyPressed(Key.SPACE) || inputs.isTouched(TouchSignal.TOUCH1) != null }
     )
 
-    fun update(delta: Float) {
+    fun update(delta: Seconds) {
         jumpingCharge.update(delta)
         if (jumpingCharge.grounded) {
             setTranslate(y = -10f)
@@ -38,7 +40,7 @@ class Player(private val model: Cube = Cube("player")) : CanMove by model, CanDr
 
 class Obstacle(private val model: Model) : CanDraw by model, CanMove by model {
 
-    fun update(delta: Float) {
+    fun update(delta: Seconds) {
         translate(-speed * delta)
 
         // off the screen
@@ -53,6 +55,25 @@ class Obstacle(private val model: Model) : CanDraw by model, CanMove by model {
     }
 }
 
+class Score(private val text: Text) : CanDraw by text, CanMove by text {
+
+    private var time: Int = 0
+
+    fun update(delta: Seconds) {
+        time += (delta * 100).toInt()
+
+        val seconds = (time / 100f).toInt()
+        val millis = time - (seconds * 100)
+        val m = if (millis < 10) {
+            "0$millis"
+        } else {
+            "$millis"
+        }
+        text.text = "$seconds:$m"
+    }
+}
+
+@ExperimentalStdlibApi
 class DemoGame : Game {
 
     override val worldSize: WorldSize = WorldSize(200, 200)
@@ -64,19 +85,34 @@ class DemoGame : Game {
         far = 200f
     )
 
+    private val cameraGUI = Camera2D.orthographic(
+        width = 5f, height = 5f
+    )
+
     private val shader = DefaultShaders.create3d()
 
-    @ExperimentalStdlibApi
+    private val gui = DefaultShaders.create2d()
+
     private val model by fileHandler.get<Model>("monkey.protobuf")
+
+    private val text by fileHandler.get<Text>("font")
 
     private val player = Player()
 
     private var obstacles = emptyList<Obstacle>()
 
-    @ExperimentalStdlibApi
+    lateinit var score: Score
+
     override fun create() {
         camera.translate(0f, 0f, -50f)
         player.setTranslate(-10f, -10f, 0f)
+
+        cameraGUI.translate(x = 2.5f, y = 2.5f, z = 0f)
+
+        score = Score(text).apply {
+            text.text = "toto"
+            text.setTranslate(-400, -400, 0)
+        }
 
         (0 until 10).forEach {
             val obstacle = Obstacle(model.copy()).apply {
@@ -93,15 +129,23 @@ class DemoGame : Game {
     }
 
     override fun render(delta: Seconds) {
+        // -- act --
         player.update(delta)
         obstacles.forEach { it.update(delta) }
+        score.update(delta)
 
+        // -- draw --
         shader.render { shader ->
             clear(178 / 255f, 235 / 255f, 242 / 255f)
             camera.draw(shader)
             player.draw(shader)
 
             obstacles.forEach { it.draw(shader) }
+        }
+
+        gui.render { shader ->
+            cameraGUI.draw(shader)
+            score.draw(shader)
         }
     }
 }
