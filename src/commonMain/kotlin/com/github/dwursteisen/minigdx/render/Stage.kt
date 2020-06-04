e31b0c24386308dbfdb58c8a1e0d0ceb2fec1a1e
David Wursteisen <david.wursteisen+git@gmail.com>
2020-06-04 19:20:14 +0200

    [COMMON] API rendering

--- src/commonMain/kotlin/com/github/dwursteisen/minigdx/render/Stage.kt
+++ src/commonMain/kotlin/com/github/dwursteisen/minigdx/render/Stage.kt
@@ -4,27 +4,26 @@ import com.github.dwursteisen.minigdx.ecs.EntityQuery
 import com.github.dwursteisen.minigdx.shaders.FragmentShader
 import com.github.dwursteisen.minigdx.shaders.VertexShader
 
+data class RenderOptions(
+    val renderName: String,
+    var renderOnDisk: Boolean
+)
+
 interface Stage
 
-interface Interceptor {
-
-    fun intercept(stage: Stage)
-}
-
 data class RenderStage(
-    val name: String,
     val vertex: VertexShader,
     val fragment: FragmentShader,
     val query: EntityQuery,
-    val publisher: Interceptor? = null
+    val renderOnScreen: Boolean = true,
+    val renderOption: RenderOptions
 ) : Stage
 
 data class MixStage(
-    val name: String,
-    val reference: Stage,
     val fragment: FragmentShader,
     val query: EntityQuery,
-    val publisher: Interceptor? = null
+    val renderOnScreen: Boolean = true,
+    val renderOption: RenderOptions
 ) : Stage
 
-class RenderManager
+class FrameBufferRegistry
