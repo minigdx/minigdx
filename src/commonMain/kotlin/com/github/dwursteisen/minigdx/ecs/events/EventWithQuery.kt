de5617fd1eab6e45126b1a6b44cd69c37ffc97a3
David Wursteisen <david.wursteisen+git@gmail.com>
2020-08-22 20:26:07 +0200

    [ECS] Create an ObjectPool

--- src/commonMain/kotlin/com/github/dwursteisen/minigdx/ecs/events/EventWithQuery.kt
+++ src/commonMain/kotlin/com/github/dwursteisen/minigdx/ecs/events/EventWithQuery.kt
@@ -6,9 +6,29 @@ internal class EventWithQuery(var event: Event, var entityQuery: EntityQuery? = 
 
     companion object {
 
-        // TODO: Can be create from an object Pool.
+        private class NoEvent : Event
+
+        private class EventWithQueryObjectPool : ObjectPool<EventWithQuery>(10) {
+
+            override fun newInstance(): EventWithQuery {
+                return EventWithQuery(NO_EVENT, null)
+            }
+
+            override fun destroyInstance(obj: EventWithQuery) {
+                obj.event = NO_EVENT
+                obj.entityQuery = null
+            }
+        }
+
+        private val NO_EVENT = NoEvent()
+
+        private val objectPool = EventWithQueryObjectPool()
+
         fun of(event: Event, entityQuery: EntityQuery? = null): EventWithQuery {
-            return EventWithQuery(event, entityQuery)
+            return objectPool.obtain().apply {
+                this.event = event
+                this.entityQuery = entityQuery
+            }
         }
     }
 }
