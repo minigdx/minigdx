c344845f440d1f7e76dbdceb123723d10b3258ad
David Wursteisen <david.wursteisen+git@gmail.com>
2021-09-07 10:33:34 +0200

    Fix issue with Text not updated correctly

--- src/commonMain/kotlin/com/github/dwursteisen/minigdx/ecs/components/text/WriteText.kt
+++ src/commonMain/kotlin/com/github/dwursteisen/minigdx/ecs/components/text/WriteText.kt
@@ -7,15 +7,18 @@ class WriteText(content: String) : TextEffect {
     override var content: String = content
         set(value) {
             field = value
-            wasUpdated = true
+            _wasUpdated = true
         }
 
     override var isFinished: Boolean = true
 
+    private var _wasUpdated: Boolean = true
+
     override var wasUpdated: Boolean = true
 
     override fun update(delta: Seconds) {
-        wasUpdated = false
+        wasUpdated = _wasUpdated
+        _wasUpdated = false
     }
 
     override fun getAlteration(characterIndex: Int): Alteration = Alteration.none
